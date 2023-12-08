27bb8c704c21340f8c2ef62eae532e6019ad064d
Adrian Baker <adrian.baker@sky.co.nz>
2023-12-08 13:51:48 +1300

    Jakarta EE 9 compatibility

--- hollow-ui-tools/src/main/java/com/netflix/hollow/ui/HttpHandlerWithServletSupport.java
+++ hollow-ui-tools/src/main/java/com/netflix/hollow/ui/HttpHandlerWithServletSupport.java
@@ -54,18 +54,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-import javax.servlet.ReadListener;
-import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.WriteListener;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-import javax.servlet.http.HttpUtils;
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.WriteListener;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 
 
 public class HttpHandlerWithServletSupport implements HttpHandler {
@@ -215,7 +214,7 @@ public class HttpHandlerWithServletSupport implements HttpHandler {
         }
 
         @Override
-        public javax.servlet.ServletOutputStream getOutputStream() throws IOException {
+        public jakarta.servlet.ServletOutputStream getOutputStream() throws IOException {
             return servletOutputStream;
         }
 
